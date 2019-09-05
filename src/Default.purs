282dfb87e2bd1e54536da9b3ee957ddda181f949
Michael Bock <me@thought2.de>
2019-09-05 08:07:13 +0200

    Format code

--- src/Default.purs
+++ src/Default.purs
@@ -6,10 +6,23 @@ import Data.Maybe (Maybe(Nothing))
 class Default a where
   def :: a
 
-instance defaultInt :: Default Int where def = 0
-instance defaultNumber :: Default Number where def = 0.0
-instance defaultMaybe :: Default (Maybe a) where def = Nothing
-instance defaultString :: Default String where def = ""
-instance defaultBoolean :: Default Boolean where def = false
-instance defaultArray :: Default (Array a) where def = mempty
-instance defaultUnit :: Default Unit where def = unit
+instance defaultInt :: Default Int where
+  def = 0
+
+instance defaultNumber :: Default Number where
+  def = 0.0
+
+instance defaultMaybe :: Default (Maybe a) where
+  def = Nothing
+
+instance defaultString :: Default String where
+  def = ""
+
+instance defaultBoolean :: Default Boolean where
+  def = false
+
+instance defaultArray :: Default (Array a) where
+  def = mempty
+
+instance defaultUnit :: Default Unit where
+  def = unit
